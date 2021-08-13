7990c082a52856daa43044a34f9423d417cb53ed
Nishant panigrahi <37862240+duanwesh@users.noreply.github.com>
2021-08-13 15:11:58 +0000

    Added new library chalk for adding color to CLI

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 
 var readLineSync=require("readline-sync")
+const chalk = require('chalk');
 var quizApp=[
   
  {
-    "question":"What's your name",
-    "answer":"Nishant Panigrahi"
+    "question":"What type of bird delivers mission instructions to Zenitsu?",
+    "answer":"Sparrow"
   },
   {
-    "question":"What's your Age",
-    "answer":"22"
+    "question":"What is not a form of Breathing used at least once in the manga series",
+    "answer":"Steel Breathing"
   },
  {
-    "question":"What's your Hobby",
-    "answer":"Coding"
+    "question":"What is the name of the blacksmith who crafts Inosuke's second pair of blades?",
+    "answer":"Kozo Kanamori"
   },
   {
-    "question":"What's your Fashion",
-    "answer":"Nothing"
+    "question":"Who always passes out while sleeping",
+    "answer":"Zenitsu"
   },
   {
-    "question":"What's your Strength",
-    "answer":"Passion"
+    "question":"Who's the head of demons",
+    "answer":"Muzan"
   }
 
 ];
 
-console.log(quizApp);
-var score=0
+var score=0,cache=0
 for(let i=0;i<quizApp.length;i++)
 {
-  //console.log("s")
-  console.log(quizApp[i].question)
+  console.log(chalk.blue.bold(quizApp[i].question))
   var ans=readLineSync.question("Enter Answer")
   if(ans===quizApp[i].answer)
   {
+    console.log(chalk.green.underline("You slayed the demon"));
     score=score+1;
-  }
 
+  }
+  else
+  {
+    
+    console.log(chalk.red.underline("You have been defeated by demon this round !"));
+    score=score-1;
+
+  }
+ console.log("Your Health is: "+score)
 }
 
-console.log("Your score is:"+score);
+if(score>0 && score===5)
+console.log(chalk.green.bold("You have defeated Muzan"));
+else 
+console.log(chalk.red.bold("Improve your technique mate Muzan is strong"));
